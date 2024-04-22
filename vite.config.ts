1f5506ef8dce7902b240fc129f7a5e1f778ed827
CanYu <fineaisa@gmail.com>
2024-04-23 01:05:25 +0800

    refactor: convert images to better format for performance

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,22 @@ import { defineConfig, type UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import viteImagemin from '@vheemstra/vite-plugin-imagemin'
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminWebp from 'imagemin-webp';
+import imageminPngquant from 'imagemin-pngquant';
 
 export default defineConfig((): UserConfig => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(),
+      viteImagemin({
+        plugins: {
+          jpg: imageminMozjpeg(),
+          png: imageminPngquant(),
+          webp: imageminWebp(),
+        },
+      }),
+    ],
     server: {
       headers: {
         "Cache-Control": "public, max-age=0",
