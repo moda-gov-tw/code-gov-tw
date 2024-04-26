3a3a870c8a8f4deb2354decbb377ac50cb28aee4
CanYu <fineaisa@gmail.com>
2024-04-26 15:12:34 +0800

    chore: resolve security issue from packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,10 @@ import { defineConfig, type UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import viteImagemin from '@vheemstra/vite-plugin-imagemin'
-import imageminMozjpeg from 'imagemin-mozjpeg';
-import imageminWebp from 'imagemin-webp';
-import imageminPngquant from 'imagemin-pngquant';
 
 export default defineConfig((): UserConfig => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths(),
-      viteImagemin({
-        plugins: {
-          jpg: imageminMozjpeg(),
-          png: imageminPngquant(),
-          webp: imageminWebp(),
-        },
-      }),
-    ],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     server: {
       headers: {
         "Cache-Control": "public, max-age=0",
