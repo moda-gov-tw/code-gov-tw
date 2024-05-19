1cc046ef408358f09fd9b68b56c5cabab59850f5
CanYu <fineaisa@gmail.com>
2024-05-20 01:11:59 +0800

    feat: Add support for browsing different pages wit url

---
+++ src/routes/projects/page-number-button.tsx
@@ -0,0 +1,50 @@
+import { $, component$, useSignal, type QRL } from "@builder.io/qwik";
+
+type Props = {
+  key: number;
+  target: number;
+  current: number;
+  onClick$: QRL<(target: number) => void>;
+};
+
+export default component$<Props>((props) => {
+  const loading = useSignal(false);
+
+  const handleButtonClick = $(async () => {
+    loading.value = true;
+    await props.onClick$(props.target);
+    loading.value = false;
+  });
+
+  return (
+    <button
+      key={props.key}
+      class={[
+        "group relative w-10 font-medium hover:text-brand-secondary",
+        props.current === props.target
+          ? "text-brand-secondary"
+          : "text-gray-300",
+        "transition-colors duration-[50ms] ease-out",
+      ]}
+      onClick$={handleButtonClick}
+      disabled={props.target === -1}
+    >
+      {props.target === -1 ? "..." : props.target}
+      <span
+        class={[
+          "absolute inset-x-0 -top-4 w-10 border-t-2 border-brand-secondary group-hover:border-brand-secondary",
+          props.current === props.target
+            ? "border-brand-secondary"
+            : "border-transparent",
+          "transition-colors duration-[50ms] ease-out",
+        ]}
+      ></span>
+      <span
+        class={[
+          "loading-icon absolute left-2.5 top-0.5 animate-spin",
+          loading.value ? "block" : "hidden",
+        ]}
+      />
+    </button>
+  );
+});
